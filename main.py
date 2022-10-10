fdd2e9d895c996814d965aa144fb0066b681816d
Vianiece <vianiece04@gmail.com>
2022-10-10 23:49:35 +0800

    update toggle js

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import os
 from pymongo import MongoClient # pip install "pymongo[srv]"
 from datetime import datetime
 import csv
+import numpy as np
+import matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import sys
 
 db_object = None
 
@@ -76,7 +81,47 @@ def get_dropdown_values(input_df = None, column_names = []):
     #     print("No columns specified in function. Aborting....")
     #     return None
     
+@eel.expose
+def heatmap_plot():
+    #this function is to plot the heatmap to predict the future price of flats based on type of flat and town area
+    #below is just hardcoded example 
+    vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
+              "potato", "wheat", "barley"]
+    farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
+           "Agrifun", "Organiculture", "BioGoods Ltd.", "Cornylee Corp."]
 
+    harvest = np.array([[0.8, 2.4, 2.5, 3.9, 0.0, 4.0, 0.0],
+                    [2.4, 0.0, 4.0, 1.0, 2.7, 0.0, 0.0],
+                    [1.1, 2.4, 0.8, 4.3, 1.9, 4.4, 0.0],
+                    [0.6, 0.0, 0.3, 0.0, 3.1, 0.0, 0.0],
+                    [0.7, 1.7, 0.6, 2.6, 2.2, 6.2, 0.0],
+                    [1.3, 1.2, 0.0, 0.0, 0.0, 3.2, 5.1],
+                    [0.1, 2.0, 0.0, 1.4, 0.0, 1.9, 6.3]])
+
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(harvest)
+
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(np.arange(len(farmers)), labels=farmers)
+    ax.set_yticks(np.arange(len(vegetables)), labels=vegetables)
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(vegetables)):
+        for j in range(len(farmers)):
+            text = ax.text(j, i, harvest[i, j],
+                       ha="center", va="center", color="w")
+
+    ax.set_title("Harvest of local farmers (in tons/year)")
+    fig.tight_layout()
+    fig.savefig('web/resources/heatmap3.jpg')
+    #plt.show()
+    # return savedfig
+    
 
 
 @eel.expose
--- web/ext.js
+++ web/ext.js
@@ -6,19 +6,19 @@ var async_counter = 0
 console.log("js file linked");
 
 
-async function query_data(){
+async function query_data() {
   //placeholder_str = "-- Any --"
   console.log("Input detected")
   input_dict = {}
   is_filter = false // flag to indicate if any filtering is performed. if not, se input_dict to null TO PREVENT ERROR
-  $('.data-query').each(function(index, element) { // iterates through every HTML element with the .data-query class value
+  $('.data-query').each(function (index, element) { // iterates through every HTML element with the .data-query class value
     // refer to HTML to see what data-field and search-type means
-    column_name = $(element).attr("data-field") 
+    column_name = $(element).attr("data-field")
     search_type = $(element).attr("search-type")
-    input_value = $(element).val() 
+    input_value = $(element).val()
 
     selected_index = $(element).prop('selectedIndex'); //check if the first value of a dropdown (the placeholder) is selected
-    if (selected_index == 0){
+    if (selected_index == 0) {
       input_value = ""
     }
     // console.log(column_name)
@@ -26,14 +26,14 @@ async function query_data(){
     // if(column_name in input_dict){
 
     // }
-    if (input_value != ""){
+    if (input_value != "") {
       is_filter = true
     }
-    input_dict[column_name] = {"value": input_value, "search_type": search_type} 
+    input_dict[column_name] = { "value": input_value, "search_type": search_type }
 
   });
 
-  if (!is_filter){ // if no filtering is done (e.g load all results when webpage opens)
+  if (!is_filter) { // if no filtering is done (e.g load all results when webpage opens)
     input_dict = null
   }
 
@@ -42,7 +42,7 @@ async function query_data(){
   datatable = $('#main-table').DataTable();
   datatable.clear().draw(); // clear all data on table
   console.log("datatable cleared")
-  
+
   async_counter += 1 // flag to indicate if there are running async functions
   let df = await eel.query_db(input_dict)()
   console.log(df)
@@ -50,63 +50,63 @@ async function query_data(){
   // //let df = await eel.query_csv(input_dict)()
   async_counter -= 1
 
-  
+
   console.log(df)
   console.log(Object.keys(df).length)
 
-  if (async_counter == 0){ // if there are no pending async functions still running (i.e. it is the last one)
-    for (row_id in df){
-        
-        if (Object.keys(df[row_id]).length == column_count){ // check if the number of values in a row matches the number of columns. just in case.
+  if (async_counter == 0) { // if there are no pending async functions still running (i.e. it is the last one)
+    for (row_id in df) {
 
-          table_values = []
-          for (var key of Object.keys(df[row_id])) {
-            //console.log(key + " -> " + df[0][key])
-            //$( "#main-table tbody tr" ).append(`<th>${df[row_id][key]}</th>`);
-            table_values.push(df[row_id][key])
-          }
-          datatable.row.add(table_values);//add a row
+      if (Object.keys(df[row_id]).length == column_count) { // check if the number of values in a row matches the number of columns. just in case.
+
+        table_values = []
+        for (var key of Object.keys(df[row_id])) {
+          //console.log(key + " -> " + df[0][key])
+          //$( "#main-table tbody tr" ).append(`<th>${df[row_id][key]}</th>`);
+          table_values.push(df[row_id][key])
         }
+        datatable.row.add(table_values);//add a row
       }
-      datatable.draw();
-      console.log("datatable populated")
+    }
+    datatable.draw();
+    console.log("datatable populated")
   }
-  
+
 
 }
 
 
-function populate_dropdown(dropdown_json) { 
+function populate_dropdown(dropdown_json) {
   //this function gets all unique values of a specified column, then compiles them in a JSON object,
   //with the general format { key_name : [unique_value1, unique_value2] }
   console.log("populating dropdown")
   console.log("dropdown values")
   console.log(dropdown_json)
   console.log(typeof dropdown_json)
-  
+
   for (var key of Object.keys(dropdown_json)) {
-    
+
     dropdown_json[key].sort()
-    for (var index in dropdown_json[key]){
-      if (key == "flat_type"){
-        $( "#inputFlatType" ).append(`<option>${dropdown_json[key][index]}</option>`);
+    for (var index in dropdown_json[key]) {
+      if (key == "flat_type") {
+        $("#inputFlatType").append(`<option>${dropdown_json[key][index]}</option>`);
       }
-      else if (key == "town"){
-        $( "#inputTown" ).append(`<option>${dropdown_json[key][index]}</option>`);
+      else if (key == "town") {
+        $("#inputTown").append(`<option>${dropdown_json[key][index]}</option>`);
       }
-      else if (key == "street_name"){
-        $( "#inputStreetName" ).append(`<option>${dropdown_json[key][index]}</option>`);
+      else if (key == "street_name") {
+        $("#inputStreetName").append(`<option>${dropdown_json[key][index]}</option>`);
       }
-      else if (key == "flat_model"){
-        $( "#inputFlatModel" ).append(`<option>${dropdown_json[key][index]}</option>`);
+      else if (key == "flat_model") {
+        $("#inputFlatModel").append(`<option>${dropdown_json[key][index]}</option>`);
       }
-      else if (key == "month"){
-        $( "#inputEarliestDate" ).append(`<option>${dropdown_json[key][index]}</option>`);
-        $( "#inputLatestDate" ).append(`<option>${dropdown_json[key][index]}</option>`);
-        
+      else if (key == "month") {
+        $("#inputEarliestDate").append(`<option>${dropdown_json[key][index]}</option>`);
+        $("#inputLatestDate").append(`<option>${dropdown_json[key][index]}</option>`);
+
       }
     }
-      }
+  }
 
 }
 
@@ -127,44 +127,44 @@ function populate_main_table(df) {
   //generate headers
   for (var key of Object.keys(df[0])) {
     //console.log(key + " -> " + df[0][key])
-    if (!(key.startsWith("_"))){  // append column header
+    if (!(key.startsWith("_"))) {  // append column header
       column_header_list.push(key)
       column_count += 1
-      $( "#main-table thead tr" ).append(`<th>${key}</th>`);
+      $("#main-table thead tr").append(`<th>${key}</th>`);
     }
     // else{
     //   exclude_column_count += 1
     // }
   }
 
-  for (row_id in df){
+  for (row_id in df) {
 
-      if ((Object.keys(df[row_id]).length) == column_count){ 
-        // just to check if the number of items in the record matches the number of columns on the table
+    if ((Object.keys(df[row_id]).length) == column_count) {
+      // just to check if the number of items in the record matches the number of columns on the table
 
-        table_values_html = ""
-        for (var key of column_header_list) {
-          //console.log(key + " -> " + df[0][key])
-          //$( "#main-table tbody tr" ).append(`<th>${df[row_id][key]}</th>`);
-          table_values_html += `<td>${df[row_id][key]}</td>`
-        }
-        $( "#main-table tbody" ).append(`<tr>${table_values_html}</tr>`);//add a row
-        
+      table_values_html = ""
+      for (var key of column_header_list) {
+        //console.log(key + " -> " + df[0][key])
+        //$( "#main-table tbody tr" ).append(`<th>${df[row_id][key]}</th>`);
+        table_values_html += `<td>${df[row_id][key]}</td>`
       }
+      $("#main-table tbody").append(`<tr>${table_values_html}</tr>`);//add a row
+
+    }
   }
 
-  main_table = $('#main-table').DataTable({ 
+  main_table = $('#main-table').DataTable({
     "pagingType": "input", //sets pagination mode
     order: [[0, 'desc']], // sets first column as descending
     language: {
       searchPlaceholder: "",
       search: "Filter existing results by text/number: ",
       "paginate": {
-        "first":      "First",
-        "last":       "Last",
-        "next":       "Next page >>",
-        "previous":   "Previous page <<"
-    },
+        "first": "First",
+        "last": "Last",
+        "next": "Next page >>",
+        "previous": "Previous page <<"
+      },
     }
   }); //datatable is declared only when the HTML for the table has been finalised
 
@@ -172,25 +172,47 @@ function populate_main_table(df) {
 
 }
 
+function displaygraph() { //prediction graph
+  eel.heatmap_plot();
+  $('#graph').append(`<img src= "/resources/heatmap.jpg">`);
+}
+
 //EVENT LISTENERS
 
-$(document).ready( function () { // runs when the webpage loads
-  console.log("document ready")
-  dropdown_column_names = ["flat_type", "town", "street_name", "flat_model", "month" ] //specify all columns in dataset to pull all unique values for
+$(document).ready(function () { // runs when the webpage loads
+  console.log("document ready");
+  dropdown_column_names = ["flat_type", "town", "street_name", "flat_model", "month"] //specify all columns in dataset to pull all unique values for
   eel.get_dropdown_values(null, dropdown_column_names)(populate_dropdown)
   console.log(1)
   eel.query_db(null)(populate_main_table)
+  // toggletabs();
+  displaygraph();
   console.log(2)
-} );
 
-$(".data-query").on("input", function() {
+});
+
+$(".data-query").on("input", function () {
   //every time an input is detected, run query
   //can consider a delay so that a query isnt run every letter (assuming this is even an issue)
   query_data()
-  
+
   //contain_values = df[df['month'].str.contains('ju')]
 
 });
+
+// function toggletabs() {
+//   // $('#graph').hide()
+//   $('#graph_display').click(function () {
+//     $("#main-table").hide();
+//     $(".tab-content").hide();
+//     $("#graph").show();
+//   });
+//   $("#main_table").click(function () {
+//     $("#main-table").show();
+//     $(".tab-content").show();
+//   });
+
+// }
 
 
 // next move most likely is to split the input for the numerical column searches to accept 2 values (to form a range)
